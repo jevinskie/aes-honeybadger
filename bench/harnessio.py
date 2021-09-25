e65c410388dbfaf7018fb56dd5d4af84f89aa8eb
Jevin Sweval <jevinsweval@gmail.com>
2021-09-24 20:31:52 -0700

    wip

--- bench/harnessio.py
+++ bench/harnessio.py
@@ -17,65 +17,7 @@ class HarnessIO(Elaboratable):
         m = Module()
         return m
 
-    # lut4 = datad ? ( datac ? ( datab ? ( dataa ? mask[15] : mask[14])
-    #                                  : ( dataa ? mask[13] : mask[12]))
-    #                        : ( datab ? ( dataa ? mask[11] : mask[10])
-    #                                  : ( dataa ? mask[ 9] : mask[ 8])))
-    #              : ( datac ? ( datab ? ( dataa ? mask[ 7] : mask[ 6])
-    #                                  : ( dataa ? mask[ 5] : mask[ 4]))
-    #                        : ( datab ? ( dataa ? mask[ 3] : mask[ 2])
-    #                                  : ( dataa ? mask[ 1] : mask[ 0])));
 
-    @classmethod
-    def simulate(cls, d0, d1, d2, d3, mask):
-        if d3:
-            if d2:
-                if d1:
-                    if d0:
-                        return mask[15]
-                    else:
-                        return mask[14]
-                else:
-                    if d0:
-                        return mask[13]
-                    else:
-                        return mask[12]
-            else:
-                if d1:
-                    if d0:
-                        return mask[11]
-                    else:
-                        return mask[10]
-                else:
-                    if d0:
-                        return mask[9]
-                    else:
-                        return mask[8]
-        else:
-            if d2:
-                if d1:
-                    if d0:
-                        return mask[7]
-                    else:
-                        return mask[6]
-                else:
-                    if d0:
-                        return mask[5]
-                    else:
-                        return mask[4]
-            else:
-                if d1:
-                    if d0:
-                        return mask[3]
-                    else:
-                        return mask[2]
-                else:
-                    if d0:
-                        return mask[1]
-                    else:
-                        return mask[0]
-
-        return 0
 
 if __name__ == "__main__":
     sclk = Signal()
