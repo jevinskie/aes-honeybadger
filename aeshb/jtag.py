e2d9ab1ba39af0f63020f2f1aea445ced8d745ce
Jevin Sweval <jevinsweval@gmail.com>
2021-09-26 13:44:22 -0700

    wip

--- aeshb/jtag.py
+++ aeshb/jtag.py
@@ -18,83 +18,83 @@ class JTAGTAPFSM(Elaboratable):
             encoding = {'test_logic_reset': 0, 'capture_dr': 1}
             state = Signal(4)
 
-        self.fsm = FakeFSM()
+        # self.fsm = FakeFSM()
 
         # Debug counter tick
         m.d.jtag += self.tck_cnt.eq(self.tck_cnt + 1)
 
-        # with m.FSM(domain="jtag", name="jtagtap") as self.fsm:
-        #     with m.State("test_logic_reset"):
-        #         with m.If(~self.tms):
-        #             m.next = "run_test_idle"
-        #     with m.State("run_test_idle"):
-        #          with m.If( self.tms):
-        #              m.next = "select_dr_scan"
-        #     # DR
-        #     with m.State("select_dr_scan"):
-        #         with m.If(~self.tms):
-        #             m.next = "capture_dr"
-        #         with m.Else():
-        #             m.next = "select_ir_scan"
-        #     with m.State("capture_dr"):
-        #         with m.If(~self.tms):
-        #             m.next = "shift_dr"
-        #         with m.Else():
-        #             m.next = "exit1_dr"
-        #     with m.State("shift_dr"):
-        #         with m.If( self.tms):
-        #             m.next = "exit1_dr"
-        #     with m.State("exit1_dr"):
-        #         with m.If(~self.tms):
-        #             m.next = "pause_dr"
-        #         with m.Else():
-        #             m.next = "update_dr"
-        #     with m.State("pause_dr"):
-        #         with m.If( self.tms):
-        #             m.next = "exit2_dr"
-        #     with m.State("exit2_dr"):
-        #         with m.If( self.tms):
-        #             m.next = "update_dr"
-        #         with m.Else():
-        #             m.next = "shift_dr"
-        #     with m.State("update_dr"):
-        #         with m.If( self.tms):
-        #             m.next = "select_dr_scan"
-        #         with m.Else():
-        #             m.next = "run_test_idle"
-        #
-        #     # IR
-        #     with m.State("select_ir_scan"):
-        #         with m.If(~self.tms):
-        #             m.next = "capture_ir"
-        #         with m.Else():
-        #             m.next = "test_logic_reset"
-        #     with m.State("capture_ir"):
-        #         with m.If(~self.tms):
-        #             m.next = "shift_ir"
-        #         with m.Else():
-        #             m.next = "exit1_ir"
-        #     with m.State("shift_ir"):
-        #         with m.If( self.tms):
-        #             m.next = "exit1_ir"
-        #     with m.State("exit1_ir"):
-        #         with m.If(~self.tms):
-        #             m.next = "pause_ir"
-        #         with m.Else():
-        #             m.next = "update_ir"
-        #     with m.State("pause_ir"):
-        #         with m.If( self.tms):
-        #             m.next = "exit2_ir"
-        #     with m.State("exit2_ir"):
-        #         with m.If( self.tms):
-        #             m.next = "update_ir"
-        #         with m.Else():
-        #             m.next = "shift_ir"
-        #     with m.State("update_ir"):
-        #         with m.If( self.tms):
-        #             m.next = "select_dr_scan"
-        #         with m.Else():
-        #             m.next = "test_logic_reset"
+        with m.FSM(domain="jtag", name="jtagtap") as self.fsm:
+            with m.State("test_logic_reset"):
+                with m.If(~self.tms):
+                    m.next = "run_test_idle"
+            with m.State("run_test_idle"):
+                 with m.If( self.tms):
+                     m.next = "select_dr_scan"
+            # DR
+            with m.State("select_dr_scan"):
+                with m.If(~self.tms):
+                    m.next = "capture_dr"
+                with m.Else():
+                    m.next = "select_ir_scan"
+            with m.State("capture_dr"):
+                with m.If(~self.tms):
+                    m.next = "shift_dr"
+                with m.Else():
+                    m.next = "exit1_dr"
+            with m.State("shift_dr"):
+                with m.If( self.tms):
+                    m.next = "exit1_dr"
+            with m.State("exit1_dr"):
+                with m.If(~self.tms):
+                    m.next = "pause_dr"
+                with m.Else():
+                    m.next = "update_dr"
+            with m.State("pause_dr"):
+                with m.If( self.tms):
+                    m.next = "exit2_dr"
+            with m.State("exit2_dr"):
+                with m.If( self.tms):
+                    m.next = "update_dr"
+                with m.Else():
+                    m.next = "shift_dr"
+            with m.State("update_dr"):
+                with m.If( self.tms):
+                    m.next = "select_dr_scan"
+                with m.Else():
+                    m.next = "run_test_idle"
+
+            # IR
+            with m.State("select_ir_scan"):
+                with m.If(~self.tms):
+                    m.next = "capture_ir"
+                with m.Else():
+                    m.next = "test_logic_reset"
+            with m.State("capture_ir"):
+                with m.If(~self.tms):
+                    m.next = "shift_ir"
+                with m.Else():
+                    m.next = "exit1_ir"
+            with m.State("shift_ir"):
+                with m.If( self.tms):
+                    m.next = "exit1_ir"
+            with m.State("exit1_ir"):
+                with m.If(~self.tms):
+                    m.next = "pause_ir"
+                with m.Else():
+                    m.next = "update_ir"
+            with m.State("pause_ir"):
+                with m.If( self.tms):
+                    m.next = "exit2_ir"
+            with m.State("exit2_ir"):
+                with m.If( self.tms):
+                    m.next = "update_ir"
+                with m.Else():
+                    m.next = "shift_ir"
+            with m.State("update_ir"):
+                with m.If( self.tms):
+                    m.next = "select_dr_scan"
+                with m.Else():
+                    m.next = "test_logic_reset"
 
         return m
 
@@ -147,16 +147,15 @@ class AlteraJTAG(Elaboratable):
         self.add_reserved_jtag_decls(platform)
         self.reserved_pads = reserved_pads = self.get_reserved_jtag_pads(platform)
 
-        m.domains.jtag = jtag = ClockDomain("jtag", async_reset=True)
-        # m.d.comb += [
-        #     ClockSignal("jtag").eq(self.tck),
-        # ]
-
-
-        m.domains.jtag_inv = jtag_inv = ClockDomain("jtag_inv", async_reset=True)
+        m.domains.jtag = jtag = ClockDomain("jtag", reset_less=True)
         m.d.comb += [
-            ClockSignal("jtag_inv").eq(~ClockSignal("jtag")),
-            ResetSignal("jtag_inv").eq(ResetSignal("jtag")),
+            jtag.clk.eq(self.tck),
+        ]
+
+
+        m.domains.jtag_inv = jtag_inv = ClockDomain("jtag_inv", reset_less=True)
+        m.d.comb += [
+            jtag_inv.clk.eq(~jtag.clk),
         ]
 
         m.submodules.tap_fsm = tap_fsm = JTAGTAPFSM(self.tms)
@@ -210,9 +209,11 @@ class AlteraJTAG(Elaboratable):
 if __name__ == "__main__":
     from nmigen_boards.arrow_deca import ArrowDECAPlatform
 
+    # topm = Module()
+    # topm.domains.jtag = jtag = ClockDomain("jtag", reset_less=True)
     # tms = Signal()
-    # tapfsm = JTAGTAPFSM(tms)
-    # main(tapfsm, ports=[tms])
+    # topm.submodules.tapfsm = tapfsm = JTAGTAPFSM(tms)
+    # main(topm, ports=[tms])
     platform = ArrowDECAPlatform()
     jtag = AlteraJTAG()
     platform.build(jtag, ports=[
