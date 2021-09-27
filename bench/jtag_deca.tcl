6347a65cc8844494b9cb5e4ce779316cd2275df7
Jevin Sweval <jevinsweval@gmail.com>
2021-09-26 21:07:48 -0700

    hello jtag works

---
+++ bench/jtag_deca.tcl
@@ -0,0 +1,28 @@
+source [find interface/altera-usb-blaster2.cfg]
+
+jtag newtap 10m50 tap -expected-id 0x031050dd -irlen 10
+set _CHIPNAME 10m50
+
+source [find cpld/jtagspi.cfg]
+
+
+gdb_port disabled
+tcl_port disabled
+telnet_port disabled
+
+init
+
+irscan 10m50.tap 0xe
+
+puts "Waiting for Any Key"
+gets stdin
+
+puts "  DEADBEEF 32 RES: 0x[drscan 10m50.tap 32 0xDEADBEEF]"
+
+puts "00DEADBEEF 40 RES: 0x[drscan 10m50.tap 40 0x00DEADBEEF]"
+
+puts "00DEADBEEF 64 RES: 0x[drscan 10m50.tap 64 0x00000000DEADBEEF]"
+
+puts "00DEADBEEF 80 RES: 0x[drscan 10m50.tap 80 0x000000000000DEADBEEF]"
+
+shutdown
